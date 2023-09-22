68b46396737cef1c17b2dd5a27ae13e283b809a7
NicoleUT <130813714+NicoleUT@users.noreply.github.com>
2023-09-22 19:07:46 -0400

    Optimize Random main.js

--- js/main.js
+++ js/main.js
@@ -1101,7 +1101,9 @@ window.onload = function(){
                             action = 30 + Math.random() * 3;
                             break;
                         case "o":
-                            action = 15 + Math.random() * 3;
+                            let randomBytes = new Uint8Array(1);
+                            crypto.getRandomValues(randomBytes);
+                            action = 15 + (randomBytes[0] / 255) * 3;
                             break;
                         default:
                             // Manejar el caso en el que actionValue no coincida con ningún caso
