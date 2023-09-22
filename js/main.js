128dc8cfa29cbafb9080b0f6ab3371a13cb6fe93
Marco <marco.escobar@ucb.edu.bo>
2023-09-22 15:29:08 -0400

    Fix condicional

--- js/main.js
+++ js/main.js
@@ -51,22 +51,11 @@ window.onload = function(){
     }
 
     function updateSquarecolor(){
-        for(var n = 0; n < 64; n++){
-            if(Math.floor(n/8)%2 === 0){
-                if(n%2 === 0){
-                    sqs[n].style.background = '#9ff';
-                }
-                else {
-                    sqs[n].style.background = '#5fa';
-                }
-            }
-            else {
-                if(n%2 === 1){
-                    sqs[n].style.background = '#9ff';
-                }
-                else {
-                    sqs[n].style.background = '#5fa';
-                }
+        for(let n = 0; n < 64; n++){
+            if (Math.floor(n / 8) % 2 === 0) {
+                sqs[n].style.background = n % 2 === 0 ? '#9ff' : '#5fa';
+            } else {
+                sqs[n].style.background = n % 2 === 1 ? '#9ff' : '#5fa';
             }
         }
     }
